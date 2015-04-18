8d59ff62bd38c34480004374004cd198239dadac
lingyics <lingyics@gmail.com>
2015-04-18 00:56:31 -0400

    adding the first part

---
+++ SendEmail.py
@@ -0,0 +1,41 @@
+'''
+Sending Emails using Python is Simple and Efficient
+We could send text, html, attachment,image, audio and ect
+Simple sample code
+
+'''
+
+import os
+import os.path
+import sys
+from ftplib import FTP
+import zipfile
+from zipfile import *
+import shutil
+import time
+from datetime import datetime
+import MySQLdb
+import MySQLdb.cursors
+import csv
+import glob
+import decimal
+import getpass
+
+
+import time
+import datetime
+from datetime import datetime
+from time import strptime, strftime
+from datetime import date, timedelta
+
+# this is the Library we use
+import smtplib
+import mimetypes
+from email.mime.multipart import MIMEMultipart
+from email import encoders
+from email.message import Message
+from email.mime.audio import MIMEAudio
+from email.mime.base import MIMEBase
+from email.mime.image import MIMEImage
+from email.mime.text import MIMEText
+
